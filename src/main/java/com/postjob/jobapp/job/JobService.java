34fc41575570fb9fb6c47dc103d3ca1fc0e2bc15
Pasindu599 <pasindusankalpa599@gmail.com>
2025-08-20 15:50:40 +0530

    Added job controller and service

---
+++ src/main/java/com/postjob/jobapp/job/JobService.java
@@ -0,0 +1,18 @@
+package com.postjob.jobapp.job;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface JobService {
+
+    List<Job> findAll();
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+
+    Boolean deleteJobById(Long id);
+
+    Boolean updateJob(Long id , Job updatedjob);
+}
---
+++ src/main/java/com/postjob/jobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,70 @@
+package com.postjob.jobapp.job.impl;
+
+import com.postjob.jobapp.job.Job;
+import com.postjob.jobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+
+import java.security.cert.TrustAnchor;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs  = new ArrayList<>();
+
+    private Long nextId = 1L;
+
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for ( Job job : jobs){
+            if  ( job.getId().equals(id)){
+                return job;
+             }
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()){
+            Job job = iterator.next();
+            if ( job.getId().equals(id)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean updateJob(Long id, Job updatedJob) {
+        for ( Job job : jobs) {
+            if ( job.getId().equals(id)){
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription(updatedJob.getDescription());
+                job.setLocation(updatedJob.getLocation());
+                job.setMinSalary(updatedJob.getMinSalary());
+                job.setMaxSalary(updatedJob.getMaxSalary());
+                return true;
+            }
+        }
+        return false;
+    }
+}
